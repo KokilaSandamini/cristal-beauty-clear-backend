8da05a844ff7a1de814eef95df39c96680778842
KokilaSandamini <sandaminikokila315@gmail.com>
2025-04-05 17:58:36 +0530

    dotenv implementation

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import userRouter from './routes/userRouter.js';
 import productRouter from './routes/productRouter.js';
 import verifyJWT from './middleware/auth.js';
 import orderRouter from './routes/orderRouter.js';
+import dotenv from 'dotenv';
+dotenv.config()
 
 let app = express();
 
-mongoose.connect("mongodb+srv://admin:123@cluster0.l9vfy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
+mongoose.connect(process.env.MONGO_UR).then(
     ()=>{
         console.log("Connected to the database");
     }
